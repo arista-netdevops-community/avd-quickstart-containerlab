27db1d4c21d7e26f700705041272f86ddadf9901
Petr Ankudinov <pa@arista.com>
2021-12-08 13:25:50 +0000

    remove legacy files

--- create_venv.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-sudo sudo apt install python3.9
-sudo apt install python3.9-venv
-python3.9 -m venv .venv
-source .venv/bin/activate
-pip install -r requirements.txt
-shopt -s expand_aliases
-alias venv="source .venv/bin/activate"
