d04ced39d0c14079f1dfa0ef52f0510db4925b60
Petr Ankudinov <pa@arista.com>
2022-04-09 08:33:54 +0000

    create add_aliases

---
+++ .cookiecutters/{{ cookiecutter.in.avd_general.avd_repository_name }}/add_aliases.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+
+{% for a_ceos_container in cookiecutter.in.inventory %}
+alias {{ a_ceos_container.hostname }}='sshpass -p {{ cookiecutter.in.avd_mgmt.cvp_password }} ssh -o "StrictHostKeyChecking no" {{ cookiecutter.in.avd_mgmt.cvp_username }}@{{ a_ceos_container.management_ip }}'
+{% endfor %}
